150a8c86d3b691336c88e4b78ead98ba80edffb5
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-08 12:00:38 -0500

    Initial commit

---
+++ app/src/main/java/g620apps/imagegallery/MainActivity.java
@@ -0,0 +1,77 @@
+package g620apps.imagegallery;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends Activity {
+
+    // the images to display
+    Integer[] imageIDs = { R.drawable.phone, R.drawable.camera, R.drawable.home,
+            R.drawable.profilepic, R.drawable.refresh, R.drawable.home, R.drawable.addpic,
+            R.drawable.profilepic, R.drawable.refresh, R.drawable.camera };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Note that Gallery view is deprecated in Android 4.1---
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getBaseContext(),
+                        "pic" + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+                // display the images selected
+                ImageView imageView = (ImageView) findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        private int itemBackground;
+
+        public ImageAdapter(Context c) {
+            context = c;
+
+        }
+
+        // returns the number of images
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        // returns the ID of an item
+        public Object getItem(int position) {
+            return position;
+        }
+
+        // returns the ID of an item
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // returns an ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }
+}
